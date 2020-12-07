b3d1e8d0d5cf7a69276489d1485fa02636ab83b7
jotavf <jotavfalcao@gmail.com>
2020-12-07 00:20:18 -0300

    exercício 2 - correções

--- Product.java
+++ Product.java
@@ -1,39 +1,15 @@
 package atividade_1;
-/*Exercício 1
-• Implemente uma classe Produto
-
-
-*/
+//Exercício 1
 
 public class Product {
-    /*
-    • atributos:
-        • código: código do produto (private int) X  
-        • descrição: descrição do produto (private String) X
-        • valor de compra (private double) X
-        • custo: custo para armazenar e/ou beneficiar o produto (private double) X
-        • margem de lucro: margem de lucro desejada, utilizado para calcular o preço de venda do produto,
-        (private double)
-        • quantidade em estoque: quantidade de unidades do produto em estoque (private int)
-    */
+    
     private int codeProduct;
     private String description;
     private double priceProduct;
     private double storageCost;
     private double profitOverProduct;
     private int quantityStoragedProduct;
-    /*
-    • métodos:
-        • void compra(int quant){qtd_estoque = qtd_estoque + quant;}
-        • void venda(int quant){qtd_estoque = qtd_estoque - quant;}
-        • double calculaPrecoVenda( ){returrn vl_compra + custo + margem_lucro*(vl_compra + custo) }
-        • crie métodos getAtributo e setAtributo para os atributos: valor de compra, custo e margem de lucro
-        • crie um construtor para inicializar o valor dos atributos: código e descrição
-        • crie um construtor para inicializar o valor dos atributos: código, descrição, valor de compra, custo e
-        margem de lucro
-        • crie um construtor para inicializar o valor dos atributos: código, descrição, valor de compra, custo,
-        margem de lucro e quantidade em estoque
-    */
+
     void buyProduct(int quant){
         quantityStoragedProduct = quantityStoragedProduct + quant;
     }
--- RevendaComArray.java
+++ RevendaComArray.java
@@ -3,14 +3,24 @@ package atividade_1;
 public class RevendaComArray {
     private Product products[];
     private int index;
+    private String defaultErrorMsg = "Código do produto não pôde ser encontrado";
 
     public RevendaComArray(int quant) {
         products = new Product[quant];
     }
 
     public void insertProduct(Product product) {
-        index = 0;
-
+        int length = products.length;
+        for(index = 0; index < length;){
+            if(products[index] != null){
+                index++;
+            }
+        }
+        if(index < length){
+            products[index] = product;
+        }
+        else
+            System.out.println("Sem espaço disponível no estoque");
     }
 
     public void buyProduct(int codeProduct, int quant) {
@@ -23,7 +33,7 @@ public class RevendaComArray {
                     product.setQuantityStoragedProduct(qnt_storage + quant);
                 }
             } else
-                System.out.println("Código do produto não pôde ser encontrado");
+                System.out.println(defaultErrorMsg);
         }
     }
 
@@ -37,7 +47,7 @@ public class RevendaComArray {
                     product.setQuantityStoragedProduct(qnt_storage - quant);
                 }
             } else
-                System.out.println("Código do produto não pôde ser encontrado");
+                System.out.println(defaultErrorMsg);
         }
     }
 
@@ -46,14 +56,14 @@ public class RevendaComArray {
         double priceProduct = 0;
         for (index = 0; index < length; index++) {
             Product product = products[index];
-            int codeSearched = product.getCodeProduct();
             if (product != null) {
+                int codeSearched = product.getCodeProduct();
                 if (codeSearched == codeProduct) {
                     priceProduct = product.getPriceProduct();
                     System.out.println("Preço: " + priceProduct);
                 }
             } else
-                System.out.println("Código do produto não pôde ser encontrado");
+                System.out.println(defaultErrorMsg);
         }
         return priceProduct;
     }
